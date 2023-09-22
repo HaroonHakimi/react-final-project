50596ffcc31019bf6b3f2bef1444a5f9a64955c6
Haroon <haroonhakimi2005@gmail.com>
2023-09-22 12:45:27 -0400

    Worked on MovieInfo.jsx

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -7,15 +7,18 @@ function MovieInfo() {
   const { id } = useParams;
   const [movieInfo, setMovieInfo] = useState([]);
   const [loading, setLoading] = useState();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   async function getMovieInfo() {
     setLoading(true);
     const { data } = await axios.get(
       `https://www.omdbapi.com/?i=${id}&apikey=5c0a8d0`
     );
+    if (Array.isArray(data.Search))
+    {
       setMovieInfo(data);
       setLoading(false);
+    }
   }
 
   useEffect(() => {
@@ -26,26 +29,26 @@ function MovieInfo() {
     <>
       <div className="footer__container">
         <div className="row row__info">
-        <div className="movie__info-button--box" onClick={() => navigate(-1)}>
-            <FontAwesomeIcon icon="fa-solid fa-arrow-left"/>
-            <button>Go <span className="red">Back</span></button>
-        </div>
-        <div className="movie__description--wrapper">
-          <figure className="movie__img--wrapper">
-            <img src={movieInfo.Poster} alt="" className="movie__img" />
-          </figure>
-          <div className="movie__info--description">
-          <h1>{}</h1>
-            <h2>Genre: </h2>
-            <h2>Starring: </h2>
-            <h2>Rating: </h2>
-            <div className="movie__plot">
-              <h2>Description</h2>
-              <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque, autem!</h3>
+          <div className="movie__info-button--box" onClick={() => navigate(-1)}>
+            <FontAwesomeIcon icon="fa-solid fa-arrow-left" />
+            <button>
+              Go <span className="red">Back</span>
+            </button>
+          </div>
+            <div className="movie__description--wrapper">
+              <figure className="movie__img--wrapper">
+                <img src={movieInfo.Poster} alt="" className="movie__img" />
+              </figure>
+              <div className="movie__info--description">
+                <h1>{}</h1>
+                <h2>Genre: {movieInfo.Genre}</h2>
+                <h2>Starring: {movieInfo.Actors}</h2>
+                <div className="movie__plot">
+                  <h2>Description</h2>
+                  <h3>{movieInfo.Plot}</h3>
+                </div>
+              </div>
             </div>
-          </div>
-          </div>
-
         </div>
       </div>
     </>
